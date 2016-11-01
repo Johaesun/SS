a424b7ebca323a8c76b9112de6248d4b4b6efb45
Johaesun <991015a@naver.com>
2016-11-01 21:08:26 +0900

    ss

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $pdo = new PDO( "mysql:host=127.0.0.1; dbname=board; charset=utf8;" , "root" , "");
+        $pdo = new PDO( "mysql:host=127.0.0.1; dbname=ssdb; charset=utf8;" , "root" , "");
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     }
